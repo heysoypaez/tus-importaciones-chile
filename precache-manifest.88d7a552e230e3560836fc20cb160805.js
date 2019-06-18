e8fdf6e70a279e7cab30cc1fe5d5795ef4c24c7b
Daniel Páez <danielpaezsw66@gmail.com>
2019-06-17 21:39:37 -0400

    Updates

---
+++ precache-manifest.88d7a552e230e3560836fc20cb160805.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/tus-importaciones-chile/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/tus-importaciones-chile/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "4b93446cf9abb836dbe76e3d0b5e0e65",
+    "url": "/tus-importaciones-chile/static/media/checked.4b93446c.svg"
+  },
+  {
+    "revision": "f46f878bd29e4b446e18c709beeb0016",
+    "url": "/tus-importaciones-chile/static/media/airplane-globe.f46f878b.png"
+  },
+  {
+    "revision": "de481b9f6e8cc20375e7",
+    "url": "/tus-importaciones-chile/static/js/runtime~main.de481b9f.js"
+  },
+  {
+    "revision": "0e55bf5b32a37da2786f",
+    "url": "/tus-importaciones-chile/static/js/main.0e55bf5b.chunk.js"
+  },
+  {
+    "revision": "c83d9973842b7c2add21",
+    "url": "/tus-importaciones-chile/static/js/2.c83d9973.chunk.js"
+  },
+  {
+    "revision": "0e55bf5b32a37da2786f",
+    "url": "/tus-importaciones-chile/static/css/main.33d4ab5c.chunk.css"
+  },
+  {
+    "revision": "8b5ff07d0b97ea6f88c94cc6bf4fccdb",
+    "url": "/tus-importaciones-chile/index.html"
+  }
+];
